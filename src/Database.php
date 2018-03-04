05d04468cba8bc99d8d4a96bae00672ce6733fa2
Tah Teche <tahteche@gmail.com>
2018-03-04 00:20:40 -0700

    Adds Database->post() for creation of new Database in Barrel.

---
+++ src/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Barrel;
+
+use GuzzleHttp\Client;
+
+class Database
+{
+	private $uri;
+
+	private $db;
+
+	public function __construct($server_uri, $db_name) 
+	{
+		$this->uri = $server_uri;
+		$this->db = $db_name;
+	}
+
+	public function post()
+	{
+		$body_array = ['database_id' => $this->db];
+		$body = json_encode($body_array);
+
+		$headers = [
+						'Content-Type' => 'application/json',
+						'Accept' => 'application/json'
+					];
+
+		$client = new Client();
+		$res = $client->post($this->uri, ['headers' => $headers,'body' => $body]);
+	}
+}
