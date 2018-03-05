5b4ab202ea57db6db43801af8a0a0c01c746f2e1
Tah Teche <tahteche@gmail.com>
2018-03-05 11:29:03 -0700

    Add test for Database->get()

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -6,28 +6,33 @@ use Barrel\Database;
 final class DatabaseTest extends TestCase
 {
 	private $db;
+	private $server_url = 'http://localhost:7080/dbs';
+	private $db_name = 'test123456';
 
 	protected function setUp()
 	{
-		$this->db = new Database('http://localhost:7080/dbs', 'test123456');
+		$this->db = new Database($this->server_url, $this->db_name);
 	}
 
 	public function testPostDatabase()
 	{
 		$res = $this->db->post();
 
-		$this->assertEquals('test123456', $res['database_id']);
+		$this->assertEquals($this->db_name, $res['database_id']);
 	}
 
 	public function testGetDatabase()
 	{
-		$this->assertInstanceOf(Database::class, $db);
-		
+		$res = $this->db->get();
+
+		$this->assertEquals($this->db_name, $res['id']);
 	}
 
-	public function testGetListOfDatabases()
+	public function testGetListOfAllDatabases()
 	{
-		$this->assertInstanceOf(Database::class, $db);
+		$res = $this->db->getAll();
+
+		$this->assertContains($this->db_name, $res);
 	}
 
 	public function testDeleteDatabase()
