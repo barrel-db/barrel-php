4c80fe6605e3ae74b35ce46414e2b673c495ed94
Tah Teche <tahteche@gmail.com>
2018-03-07 00:29:54 -0700

    Implement Database->getDoc()
    Used to get a single document using its ID.

--- src/Database.php
+++ src/Database.php
@@ -9,7 +9,7 @@ class Database
 	private $server_url;
 	private $db_name;
 	private $db_url;
-	private $docs_post_url;
+	private $docs_url;
 
 	private $post_headers = [
 						'Content-Type' => 'application/json',
@@ -21,7 +21,7 @@ class Database
 		$this->server_url = $server_url;
 		$this->db_name = $db_name;
 		$this->db_url = $this->server_url . '/' . $this->db_name;
-		$this->docs_post_url = $this->db_url . '/docs';
+		$this->docs_url = $this->db_url . '/docs';
 	}
 
 	//Database Functions
@@ -71,10 +71,20 @@ class Database
 		$body = json_encode($doc);
 
 		$client = new Client();
-		$res = $client->post($this->docs_post_url, [
+		$res = $client->post($this->docs_url, [
 					'headers' => $this->post_headers,
 					'body' => $body
 				]);
+
+		return json_decode((string) $res->getBody(), true);
+	}
+
+	public function getDoc($doc_id)
+	{
+		$get_doc_url = $this->docs_url . '/' . $doc_id;
+
+		$client = new Client();
+		$res = $client->get($get_doc_url);
 
 		return json_decode((string) $res->getBody(), true);
 	}
